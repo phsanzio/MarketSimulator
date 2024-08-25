7732198b24d1bc6037fd84a21886bfedc00c157c
Pedro Sanzio <72276805+phsanzio@users.noreply.github.com>
2024-08-25 19:05:00 -0300

    working

--- cliente.py
+++ cliente.py
@@ -1,15 +1,51 @@
 from socket import *
+import pickle
+
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
+initial = input('Digite 1 para começar... ')
+clientSocket.send(initial.encode())
+server_sentence = clientSocket.recv(1024).decode()
+print(server_sentence)
+
+def list_products(dict):
+    print('\n\033[1;34m--------------------')
+    print('Lista de produtos:')
+    for product, price in dict.items():
+            print(f'{product.capitalize()}: R${price}')
+    print('--------------------\033[0m\n')
 
 while True:
     sentence = input('Digite: ')
-    clientSocket.send(sentence.encode())
-    if sentence.lower() == 'sair':
-        break
-    server_sentence = clientSocket.recv(1024).decode()
-    print(server_sentence)
+    match int(sentence):
+        case 1:
+            clientSocket.send(sentence.encode())
+            server_sentence = clientSocket.recv(1024)
+            received_dict = pickle.loads(server_sentence)
+            list_products(received_dict)
+            sentence = input('Digite 1 para ir ao menu: ')
+            clientSocket.send(sentence.encode())
+            server_sentence = clientSocket.recv(1024).decode()
+            print(server_sentence)
+        case 2:
+            clientSocket.send(sentence.encode())
+            server_sentence = clientSocket.recv(1024).decode()
+            print(server_sentence)
+            sentence = input('Digite: ')
+            clientSocket.send(sentence.encode())
+            server_sentence = clientSocket.recv(1024).decode()
+            print(server_sentence)
+            break
+        case 3:
+            pass
+        case 4:
+            clientSocket.send(sentence.encode())
+            break
+    
+    
+#     server_sentence = clientSocket.recv(1024).decode()
+#     print(server_sentence)
 
 clientSocket.close()
--- servidor.py
+++ servidor.py
@@ -1,42 +1,60 @@
 from socket import *
+import pickle
+
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 products = {'arroz': '4.00', 'frango':'5.00'}
 connectionSocket, addr = serverSocket.accept()
-print("O servidor esta pronto esperando mensagens")
+initial = connectionSocket.recv(1024).decode()
+connectionSocket.send('1 - Listar produtos'.encode())
+print("O cliente conectou ao servidor...")
 
 def menu():
-    connectionSocket.send('1 - Listar produtos\n2 - Fazer oferta'.encode())
+    connectionSocket.send('1 - Listar produtos\n2 - Comprar\n3 - Fazer oferta\n4 - Sair'.encode())
 
-def list_products(products):
-    connectionSocket.send('----------------\n'.encode())
-    connectionSocket.send('Lista de produtos:\n'.encode())
-    for product, price in products.items():
-        connectionSocket.send((f'{product.capitalize()}: R${price}\n').encode())
-    connectionSocket.send('----------------\n'.encode())
+# def list_products(products):
+#     #connectionSocket.send('----------------\n'.encode())
+#     #connectionSocket.send('Lista de produtos:\n'.encode())
+#     for product, price in products.items():
+#         connectionSocket.send((f'{product.capitalize()}: R${price}\n').encode())
+#     #connectionSocket.send('----------------\n'.encode())
+#     #menu()
         
 def oferta(teste):
     #produto = connectionSocket.recv(1024).decode()
     connectionSocket.send(products[teste].encode())
 
 
-menu()
-
 while True:
-    #menu()
     sentence = connectionSocket.recv(1024).decode()
-    print(sentence)
-    if sentence.lower() == 'sair':
-        break
-    else:
-        if sentence == '1':
-            list_products(products)
-        elif sentence == '2':
-            connectionSocket.send('Produto?'.encode())
+    #sentence = int(sentence)
+    match int(sentence):
+        case 1:
+            connectionSocket.send(pickle.dumps(products))
+            connectionSocket.recv(1024).decode()
+            menu()
+        case 2:
+            connectionSocket.send('Informe o nome do produto que deseja comprar...'.encode())
             sentence = connectionSocket.recv(1024).decode()
-            oferta(sentence)
+            if sentence.lower() in products.keys():
+                connectionSocket.send('Compra realizada! Volte sempre!'.encode())
+                print(f'O cliente efetuou a compra de {sentence.lower().capitalize()}...')
+            else:
+                connectionSocket.send('Produto não existe!'.encode())
+                print(f'O cliente não conclui a compra de {sentence.lower().capitalize()}...')
+            print("O cliente desconectou do servidor...")
+            break
+        case 3:
+            pass
+        case 4:
+            print("O cliente desconectou do servidor...")
+            break
+    #menu()
+    # sentence = connectionSocket.recv(1024).decode()
+    # print(sentence)
+    
 
 connectionSocket.close()
 serverSocket.close()
