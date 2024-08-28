760568de0176495fe8f5d3eaee6c9a49b78f60fb
Pedro Sanzio <72276805+phsanzio@users.noreply.github.com>
2024-08-28 10:57:07 -0300

    finished

--- cliente.py
+++ cliente.py
@@ -5,17 +5,17 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-initial_sentence = input('Digite 1 para começar... ')
+initial_sentence = input('\033[1;35mDigite 1 para começar...\033[0m')
 clientSocket.send(initial_sentence.encode())
 server_sentence = clientSocket.recv(1024).decode()
 print(server_sentence)
 
 def list_products(dict):
-    print('\n\033[1;34m--------------------')
+    print('\n\033[1;34m------------------------')
     print('Lista de produtos:')
     for product, price in dict.items():
-            print(f'{product.capitalize()}: R${price}')
-    print('--------------------\033[0m\n')
+            print(f'{product} - {price[0]}: R${price[1]}')
+    print('------------------------\033[0m\n')
 
 while True:
     client_sentence = input('Digite: ')
@@ -36,7 +36,10 @@ while True:
             product_buy = input('Digite: ')
             clientSocket.send(product_buy.encode())
             server_sentence = clientSocket.recv(1024).decode()
-            print(server_sentence)
+            if server_sentence == 'Produto não existe!':
+                print('\033[1;31m' + server_sentence + '\033[0m')
+            else:    
+                print('\033[1;32m' + server_sentence + '\033[0m')
             break
         case 3:
             clientSocket.send(client_sentence.encode())
@@ -45,28 +48,32 @@ while True:
             product_offer = input('Digite: ')
             clientSocket.send(product_offer.encode())
             server_sentence = clientSocket.recv(1024).decode()
+            if product_offer not in received_dict.keys():
+                print('\033[1;31m' + server_sentence + '\033[0m')
+                break
+            print('\033[1;34m' + f'{product_offer} - {received_dict[product_offer][0]}: R${received_dict[product_offer][1]}' + '\033[0m')
             print(server_sentence)
             price_want_buy = input('Digite: ')
             clientSocket.send(price_want_buy.encode())
             server_sentence = clientSocket.recv(1024).decode()
             contador = 0
             while server_sentence == 'Oferta recusada!':
-                print(server_sentence + ' Tente novamente...')
+                print('\033[1;31m' + server_sentence + ' Tente novamente...' + '\033[0m')
                 client_sentence = input('Pressione 1 para continuar...')
                 clientSocket.send(client_sentence.encode())
                 server_sentence = clientSocket.recv(1024).decode()
-                print(server_sentence)
+                print('\033[1;33m' + server_sentence + '\033[0m')
                 agree_deny = input('Digite: ')
                 if agree_deny.lower() == 's':
                     clientSocket.send(agree_deny.encode())
                     server_sentence = clientSocket.recv(1024).decode()
-                    print(server_sentence)
+                    print('\033[1;32m' + server_sentence + '\033[0m')
                     exit() #mudar
                 else:
                     clientSocket.send(agree_deny.encode())
                     server_sentence = clientSocket.recv(1024).decode()
                     if contador == 4:
-                        print(server_sentence)
+                        print('\033[1;31m' + server_sentence + '\033[0m')
                         exit()
                     else: 
                         print(server_sentence)
@@ -75,7 +82,7 @@ while True:
                         server_sentence = clientSocket.recv(1024).decode()
                 contador+=1
 
-            print(server_sentence)
+            print('\033[1;32m' + server_sentence + '\033[0m')
             break
         case 4:
             clientSocket.send(client_sentence.encode())
--- servidor.py
+++ servidor.py
@@ -6,24 +6,43 @@ serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
-price_sell = {'arroz': '20.00', 'frango':'18.00', 'alface':'2.00'}
-price_paid = {'arroz': '12.00', 'frango':'10.00', 'alface':'0.20'}
+price_sell = {
+    '1': ('Arroz 2kg', '20.00'), 
+    '2': ('Peito de frango 1kg','18.00'), 
+    '3': ('Alface 1und','2.00'),
+    '4': ('Banana 1kg','8.98'),
+    '5': ('Leite 1L','4.80'),
+    '6': ('Tomate 1kg','8.80'),
+    '7': ('Danoninho','9.80'),
+    '8': ('Kinder Ovo','19.50')
+}
+price_paid = {
+    '1': ('Arroz 2kg', '12.00'), 
+    '2': ('Peito de frango 1kg','10.00'), 
+    '3': ('Alface 1und','0.20'),
+    '4': ('Banana 1kg','5.60'),
+    '5': ('Leite 1L','2.00'),
+    '6': ('Tomate 1kg','3.80'),
+    '7': ('Danoninho','5.80'),
+    '8': ('Kinder Ovo','3.50')
+}
 connectionSocket, addr = serverSocket.accept()
 initial_sentence = connectionSocket.recv(1024).decode()
 connectionSocket.send('1 - Listar produtos'.encode())
-print("O cliente conectou ao servidor...")
+print('\033[1;34mO cliente conectou ao servidor...\033[0m')
 
 def menu():
     connectionSocket.send('1 - Listar produtos\n2 - Comprar\n3 - Fazer oferta\n4 - Sair'.encode())
 
 def oferta(product):
     contador = 0
-    price = float(price_sell[product])
-    price_acceptable = float(price_paid[product]) * 1.3
+    price = float(price_sell[product][1])
+    price_acceptable = float(price_paid[product][1]) * 1.3
     temp_price = 0
     connectionSocket.send('Informe o preço que deseja pagar...'.encode())
     while contador < 5:
         price_want_buy = connectionSocket.recv(1024).decode()
+        price_want_buy = price_want_buy.replace(',', '.')
         if contador > 0:
             while random_price >= temp_price:
                 random_price = random.uniform(price_acceptable, price)
@@ -39,19 +58,19 @@ def oferta(product):
             agree_deny = connectionSocket.recv(1024).decode()
             if agree_deny.lower() == 's':
                 connectionSocket.send('Compra realizada! Volte sempre!'.encode())
-                print(f'O cliente efetuou a compra de {product.lower().capitalize()}...')
+                print('\033[1;32m' + f'O cliente efetuou a compra de {price_sell[product][0]} por R${random_price:.2f}...' + '\033[0m')
                 break
             else:
                 if contador == 4:
                     connectionSocket.send('Tentativas Excedidas!'.encode())
-                    print('Cliente excedeu as tentativas!')
+                    print('\033[1;31mCliente excedeu as tentativas!\033[0m')
                     break
                 else:
                     connectionSocket.send('Informe uma nova oferta...'.encode())
         else:
             print('Valor que seria fornecido: R${:.2f}\nValor pago pelo cliente: R${}'.format(random_price, price_want_buy))
             connectionSocket.send('Oferta aceita e compra realizada! Volte sempre!'.encode())
-            print(f'O cliente efetuou a compra de {product.lower().capitalize()}...')
+            print('\033[1;32m' + f'O cliente efetuou a compra de {price_sell[product][0]}...' + '\033[0m')
             break
         contador+=1
             
@@ -64,28 +83,28 @@ while True:
             connectionSocket.recv(1024).decode()
             menu()
         case 2:
-            connectionSocket.send('Informe o nome do produto que deseja comprar...'.encode())
+            connectionSocket.send('Informe o número do produto que deseja comprar...'.encode())
             product_buy = connectionSocket.recv(1024).decode()
-            if product_buy.lower() in price_sell.keys():
+            if product_buy in price_sell.keys():
                 connectionSocket.send('Compra realizada! Volte sempre!'.encode())
-                print(f'O cliente efetuou a compra de {product_buy.lower().capitalize()}...')
+                print('\033[1;32m' + f'O cliente efetuou a compra de {price_sell[product_buy][0]} por R${price_sell[product_buy][1]}...' + '\033[0m')
             else:
                 connectionSocket.send('Produto não existe!'.encode())
-                print(f'O cliente não conclui a compra de {product_buy.lower().capitalize()}...')
-            print("O cliente desconectou do servidor...")
+                print('\033[1;31mO cliente não conclui a compra...\033[0m')
+            print("\033[1;35mO cliente desconectou do servidor...\033[0m")
             break
         case 3:
-            connectionSocket.send('Informe o nome do produto que deseja comprar...'.encode())
+            connectionSocket.send('Informe o número do produto que deseja comprar...'.encode())
             product_offer = connectionSocket.recv(1024).decode()
-            if product_offer.lower() in price_sell.keys():
+            if product_offer in price_sell.keys():
                 oferta(product_offer)
             else:
                 connectionSocket.send('Produto não existe!'.encode())
-                print(f'O cliente não conclui a compra de {product_offer.lower().capitalize()}...')
-            print("O cliente desconectou do servidor...")
+                print('\033[1;31mO cliente não conclui a compra...\033[0m')
+            print("\033[1;35mO cliente desconectou do servidor...\033[0m")
             break
         case 4:
-            print("O cliente desconectou do servidor...")
+            print("\033[1;35mO cliente desconectou do servidor...\033[0m")
             break
 
 
